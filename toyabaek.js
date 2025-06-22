e91c913dfac76d590da7fcf24abeb55f7731c1ea
ToyaOfficial <maszddf@gmail.com>
2025-06-22 11:20:34 +0800

    Update toyabaek.js

--- toyabaek.js
+++ toyabaek.js
@@ -1,1 +1,14 @@
-
+{
+  "creator": "6282353569436",
+  "secretCode": "kontol",
+  "description": "secretKey for ToyaOfficial.",
+  "lastUpdated": "Lates Version",
+  "author": "Toya Suka Ngaji",
+  "status": "Active",
+  "license": "MIT",
+  "version": "11.0",
+  "Access": [
+    "6282353569436",
+    "6287780293117"
+  ]
+    }
